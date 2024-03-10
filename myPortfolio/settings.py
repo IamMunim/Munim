4185f2424e1a06ae5a49d7fc4833d1807a469fe6
Obayed <mobayed201170@bscse.uiu.ac.bd>
2024-03-11 00:50:57 +0600

    A

--- myPortfolio/settings.py
+++ myPortfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-l4q_1!9$rz87+q8#93)tl)h5_v72p%4$50@*0(0$z2^k*!*k3l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'myPortfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
